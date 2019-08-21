0dc6a8a65f4436cb09109b026921aba902c0cc70
yungwilly <william_reyes@dlsu.edu.ph>
2019-08-21 22:19:09 +0800

    y

--- models/db.js
+++ models/db.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://littleworkers_DB:Krustykrab@littleworkersdb-nuuwn.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
-//     if(!err) {
-//         console.log('MongoDB connection success');
-//     }else{
-//         console.log('Error');
-//     }
-// });
-
-mongoose.connect('mongodb://localhost:27017/littleWorkers_Db', {useNewUrlParser: true}, (err) => {
+mongoose.connect('mongodb+srv://littleworkers_DB:Krustykrab@littleworkersdb-nuuwn.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
     if(!err) {
         console.log('MongoDB connection success');
     }else{
         console.log('Error');
     }
 });
+
+// mongoose.connect('mongodb://localhost:27017/littleWorkers_Db', {useNewUrlParser: true}, (err) => {
+//     if(!err) {
+//         console.log('MongoDB connection success');
+//     }else{
+//         console.log('Error');
+//     }
+// });
 
 require('./task.model');
 require('./user.model');
